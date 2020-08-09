bd4e506f81dbeaa3cb533cf2245c7fc5d2b00de7
Bjarne <bjarne_priebe@hotmail.com>
2020-08-09 17:00:08 +0200

    init react project with material ui

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./App.css";
+import { Button } from "@material-ui/core";
+
+function App() {
+  return <Button color="primary">Hello World</Button>;
+}
+
+export default App;
