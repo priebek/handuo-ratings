f6e674a04f3ded4670ce0c8b7b8158ffa035eec8
Bjarne <bjarne_priebe@hotmail.com>
2020-08-09 17:02:45 +0200

    add icons component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import "./App.css";
 import { Button } from "@material-ui/core";
 
 function App() {
-  return <Button color="primary">Hello World</Button>;
+  return (
+    <Button color="primary" variant="contained">
+      Hello World
+    </Button>
+  );
 }
 
 export default App;
