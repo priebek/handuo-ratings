7af8ae1e9a2819cea85c3120b88c5ed9e086aa54
Bjarne Kjær <bjarne.kjaer@LEGO.com>
2020-08-17 15:11:25 +0200

    use MaterialTable from material-table and cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,141 +1,55 @@
 import React from "react";
-import Table from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
-import TableCell from "@material-ui/core/TableCell";
-import TableContainer from "@material-ui/core/TableContainer";
-import TablePagination from "@material-ui/core/TablePagination";
-import TableRow from "@material-ui/core/TableRow";
-import Paper from "@material-ui/core/Paper";
-import DataModel from "./utility/datamodel";
-import { useStyles } from "./utility/headerstyle";
-import EnhancedTableHead from "./components/enhancedtableheader";
-import getComparator from "./utility/getcomparator";
-import { Order } from "./utility/order";
-import stableSort from "./utility/stablesort";
 import dataJson from "./data.json";
-import { Typography, Button } from "@material-ui/core";
+import { Button } from "@material-ui/core";
 import TheatersIcon from "@material-ui/icons/Theaters";
 import MusicNoteIcon from "@material-ui/icons/MusicNote";
-
-const rows = dataJson;
+import MaterialTable from "material-table";
 
 export default function App() {
-  const classes = useStyles();
-  const [order, setOrder] = React.useState<Order>("asc");
-  const [orderBy, setOrderBy] = React.useState<keyof DataModel>("episode");
-  const [selected, setSelected] = React.useState<string[]>([]);
-  const [page, setPage] = React.useState(0);
-  const [rowsPerPage, setRowsPerPage] = React.useState(25);
-
-  const handleRequestSort = (
-    event: React.MouseEvent<unknown>,
-    property: keyof DataModel
-  ) => {
-    const isAsc = orderBy === property && order === "asc";
-    setOrder(isAsc ? "desc" : "asc");
-    setOrderBy(property);
-  };
-
-  const handleSelectAllClick = (event: React.ChangeEvent<HTMLInputElement>) => {
-    if (event.target.checked) {
-      const newSelecteds = rows.map((n) => n.title);
-      setSelected(newSelecteds);
-      return;
-    }
-    setSelected([]);
-  };
-
-  const handleChangePage = (event: unknown, newPage: number) => {
-    setPage(newPage);
-  };
-
-  const handleChangeRowsPerPage = (
-    event: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    setRowsPerPage(parseInt(event.target.value, 10));
-    setPage(0);
-  };
-
-  const emptyRows =
-    rowsPerPage - Math.min(rowsPerPage, rows.length - page * rowsPerPage);
-
   return (
-    <div className={classes.root}>
-      <Paper className={classes.paper}>
-        <Typography variant="h2" align="center">
-          Ratings
-        </Typography>
-        <TableContainer>
-          <Table
-            className={classes.table}
-            aria-labelledby="tableTitle"
-            size="small"
-            aria-label="enhanced table"
-          >
-            <EnhancedTableHead
-              classes={classes}
-              numSelected={selected.length}
-              order={order}
-              orderBy={orderBy}
-              onSelectAllClick={handleSelectAllClick}
-              onRequestSort={handleRequestSort}
-              rowCount={rows.length}
-            />
-            <TableBody>
-              {stableSort(rows, getComparator(order, orderBy))
-                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                .map((row, index) => {
-                  const labelId = `enhanced-table-checkbox-${index}`;
-
-                  return (
-                    <TableRow hover tabIndex={-1} key={row.title}>
-                      <TableCell padding="checkbox"></TableCell>
-                      <TableCell
-                        component="th"
-                        id={labelId}
-                        scope="row"
-                        padding="none"
-                      >
-                        {row.episode}
-                      </TableCell>
-                      <TableCell align="right">{row.title}</TableCell>
-                      <TableCell align="right">
-                        <Button href={row.link} target="_blank">
-                          <MusicNoteIcon />
-                        </Button>
-                      </TableCell>
-                      <TableCell align="right">
-                        <Button href={row.imdb} target="_blank">
-                          <TheatersIcon />
-                        </Button>
-                      </TableCell>
-                      <TableCell align="right">{row.type}</TableCell>
-                      <TableCell align="right">{row.score}</TableCell>
-                      <TableCell align="right">{row.category}</TableCell>
-                      <TableCell align="right">{row.genre}</TableCell>
-                      <TableCell align="right">{row.year}</TableCell>
-                      <TableCell align="right">{row.broadcast}</TableCell>
-                    </TableRow>
-                  );
-                })}
-              {emptyRows > 0 && (
-                <TableRow style={{ height: 33 * emptyRows }}>
-                  <TableCell colSpan={6} />
-                </TableRow>
-              )}
-            </TableBody>
-          </Table>
-        </TableContainer>
-        <TablePagination
-          rowsPerPageOptions={[25, 50, 100]}
-          component="div"
-          count={rows.length}
-          rowsPerPage={rowsPerPage}
-          page={page}
-          onChangePage={handleChangePage}
-          onChangeRowsPerPage={handleChangeRowsPerPage}
-        />
-      </Paper>
+    <div style={{ maxWidth: "100%" }}>
+      <MaterialTable
+        options={{
+          exportButton: true,
+          grouping: true,
+          headerStyle: {
+            backgroundColor: "#01579b",
+            color: "#FFF",
+          },
+          pageSize: 25,
+          pageSizeOptions: [5, 25, 100, 1000],
+        }}
+        columns={[
+          { title: "Episode", field: "episode" },
+          { title: "Title", field: "title" },
+          {
+            title: "Link",
+            field: "link",
+            render: (x) => (
+              <Button href={x.link} target="_blank">
+                <MusicNoteIcon />
+              </Button>
+            ),
+          },
+          {
+            title: "IMDB",
+            field: "imdb",
+            render: (x) => (
+              <Button href={x.imdb} target="_blank">
+                <TheatersIcon />
+              </Button>
+            ),
+          },
+          { title: "Type", field: "type" },
+          { title: "Score", field: "score" },
+          { title: "Category", field: "category" },
+          { title: "Genre", field: "genre" },
+          { title: "Year", field: "year" },
+          { title: "Broadcast", field: "broadcast" },
+        ]}
+        data={dataJson}
+        title="Ratings"
+      />
     </div>
   );
 }
